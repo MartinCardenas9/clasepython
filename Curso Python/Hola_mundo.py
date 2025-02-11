33fd198a7506233a5a4f36569c7d2e0d9c49e872
Martin <martincardenas9910@gmail.com>
2025-02-11 00:19:32 -0500

    mi curso python

---
+++ Curso Python/Hola_mundo.py
@@ -0,0 +1,20 @@
+#hola como estas
+print("hola mundo")
+
+#tipo de datos
+print(type("esto es texto"))
+print(type(4))
+print(type(3.5))
+
+#variable
+a=4
+b=6
+c=a+b
+print(c)
+
+mi_texto = 'diecinueve'
+print(type(mi_texto))
+
+mi_numero =19
+print(mi_numero, mi_texto)
+print('mi numero favorito es:', mi_texto)
