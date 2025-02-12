c2c98b9ae5a1fe73c55edb16014a8ab833855c60
Martin <martincardenas9910@gmail.com>
2025-02-12 01:03:50 -0500

    curso d2

--- Curso Python/Hola_mundo.py
+++ Curso Python/Hola_mundo.py
@@ -1,20 +1,57 @@
 #hola como estas
-print("hola mundo")
+#print("hola mundo")
 
 #tipo de datos
-print(type("esto es texto"))
-print(type(4))
-print(type(3.5))
+#print(type("esto es texto"))
+#print(type(4))
+#print(type(3.5))
 
 #variable
-a=4
-b=6
-c=a+b
-print(c)
+#a=4
+#b=6
+#c=a+b
+#print(c)
 
-mi_texto = 'diecinueve'
-print(type(mi_texto))
+#mi_texto = 'diecinueve'
+#print(type(mi_texto))
 
-mi_numero =19
-print(mi_numero, mi_texto)
-print('mi numero favorito es:', mi_texto)
+#mi_numero =19
+#print(mi_numero, mi_texto)
+#print('mi numero favorito es:', mi_texto)
+
+#Operadores Arimeticos
+
+#print(6+3)
+#print(6-3)
+#print(6*3)
+#print(6/3)
+#print(9%3)
+#print(4 ** 3)
+#print(11 // 3) #es para division de float
+
+#print("hola " + "mundo")
+
+# OPERADORES COMPARATIVOS 
+#print(4>8)
+#print(4<8)
+#print( 4 == 8) 
+#print(4<=8)
+#print(4>=8)
+#print(4!=8) #diferente 
+
+#print("hola" > "mundo")
+#print("hola" > "bolas")
+#print("a"<"b")  #cuenta de la a - z
+#print("aaa" < "aba") #cuenta letra por letra 
+
+#OPERADORES LOGICOS
+
+#print(True and False) #= falso - and esto y esto son verdaderos
+#print(True or False) #=verdadero - or esto o esto son verdaderos 
+#print(not(4 < 6)) # not cambia la respuesta
+
+##ejercicio
+print((not(7 != 7) and 6 > 5) and ("rozar" < "rotar" or not(3 ==5)))
+print((True and True) and (False or True))
+print((True) and (True))
+print(True)
